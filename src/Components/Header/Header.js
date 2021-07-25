266a8c5f9752d4c773162a9ec88d6012f417cc6f
adamalcantara <alcantaradevelopment@gmail.com>
2021-07-25 17:39:27 -0400

    Added logo

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Header.css'
+import Logo from "../../Assets/img/logo.png"
+
+const Header = () => {
+    return (
+        <div>
+            <img src={Logo}></img>
+        </div>
+    )
+}
+
+export default Header
