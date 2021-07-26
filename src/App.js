e6541f9f59099a499bf9b71291d95c152f2669bd
adamalcantara <alcantaradevelopment@gmail.com>
2021-07-25 21:20:52 -0400

    Removed background image

--- src/App.js
+++ src/App.js
@@ -5,8 +5,10 @@ import Checker from './Components/Checker/Checker';
 function App() {
   return (
     <div className="App">
+      <div className="background">
       <Header />
       <Checker />
+      </div>
     </div>
   );
 }
